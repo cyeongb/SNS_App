087ad9e569af6083600a5d9f767f1813649e396f
cyeongb <cyeongb@gmail.com>
2022-04-11 17:47:02 +0900

    sanity schema 추가

---
+++ backend/schemas/pin.js
@@ -0,0 +1,62 @@
+// 이미지 연결 url
+
+export default {
+  name: "pin",
+  title: "Pin",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "about",
+      title: "About",
+      type: "string",
+    },
+    {
+      name: "destination",
+      title: "Destination",
+      type: "url",
+    },
+    {
+      name: "category",
+      title: "Category",
+      type: "string",
+    },
+    {
+      name: "image",
+      title: "Imgage",
+      type: "image",
+      options: { hotspot: true },
+      //hotspot :이미지에 텍스트, 이미지, 링크등이 포함된 아이콘 '대화형 이미지'
+    },
+    {
+      // 사용자
+      name: "userId",
+      title: "UserID",
+      type: "string",
+    },
+    {
+      // 게시자
+      name: "postedBy",
+      title: "PostedBy",
+      type: "postedBy",
+    },
+    {
+      // 저장
+      name: "save",
+      title: "Save",
+      type: "array",
+      of: [{ type: "save" }],
+    },
+    {
+      // 댓글
+      name: "comments",
+      title: "Comments",
+      type: "array",
+      of: [{ type: "comment" }],
+    },
+  ],
+};
