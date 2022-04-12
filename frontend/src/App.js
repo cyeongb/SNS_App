3f83d517cd843fb0cd8874bde2bd427c266a7781
cyeongb <cyeongb@gmail.com>
2022-04-12 16:43:41 +0900

    로그인 페이지, 메인페이지 생성

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,15 @@
 import "./App.css";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import Login from "../components/Login";
+import Home from "../container/Home";
+
 const App = () => {
-  return <div className="text-3xl font-bold underline">hihihiihi!!</div>;
+  return (
+<Routes>
+  <Route path="login" element={<Login />}/>
+  <Route path="/*" element={<Home />}/>
+</Routes>
+  )
 };
 
 export default App;
