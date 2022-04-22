e265aff9b6c99a9bdb8d939d13dd0157496feb16
cyeongb <cyeongb@gmail.com>
2022-04-22 20:59:02 +0900

    쿼리에따른 검색 데이터 fetch 작업

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -3,9 +3,31 @@ import { useParams } from "react-router-dom";
 import { client } from "../client";
 import MasonryLayout from "./MasonryLayout";
 import Spinner from "./Spinner";
+import { searchQuery, feedQuery } from "../utils/data";
 
 const Feed = () => {
   const [loading, setLoading] = useState(true);
+  const [pins, setPins] = useState("");
+
+  const { categoryId } = useParams(); // url 파라미터값을 여기에 저장한다.
+
+  useEffect(() => {
+    console.log("categoryId::", categoryId);
+    setLoading(true);
+    if (categoryId) {
+      // categoryId(=searchTerm) 로 각 게시물 데이터를 불러온다.
+      const query = searchQuery(categoryId);
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    } else {
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    }
+  }, [categoryId]);
 
   if (loading) return <Spinner />;
 
