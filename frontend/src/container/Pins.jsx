be2138748106cc4196035237109c13e03d5a6bc1
cyeongb <cyeongb@gmail.com>
2022-04-22 17:38:47 +0900

    Pins 컴포넌트에 route 설정

--- frontend/src/container/Pins.jsx
+++ frontend/src/container/Pins.jsx
@@ -1,11 +1,34 @@
-import React,{useState} from "react";
-import { Routes,Route } from "react-router-dom";
+import React, { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import { Navbar, Search, Feed, CreatePin, PinDetail } from "../components";
 
-
-
-
-const Pins = () => {
-  return <div>Pins</div>;
+const Pins = ({ user }) => {
+  //검색어를 다른 컴포넌트에(navbar,search)도 props로 공유하기 위해 pins 컴포넌트에 state 선언.
+  const [searchTerm, setSearchTerm] = useState("");
+  return (
+    <div className="px-2 md:px-5">
+      <div className="bg-gray-50">
+        <Navbar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      </div>
+      <div className="h-full">
+        <Routes>
+          <Route path="/" element={<Feed />} />
+          <Route path="/category/:categoryId" element={<Feed />} />
+          <Route
+            path="/pin-detail/:pinId"
+            element={<PinDetail user={user} />}
+          />
+          <Route path="/create-pin" element={<CreatePin user={user} />} />
+          <Route
+            path="/search"
+            element={
+              <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+            }
+          />
+        </Routes>
+      </div>
+    </div>
+  );
 };
 
 export default Pins;
