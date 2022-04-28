229b658e7dfa185adad3b06ceab135d058839516
cyeongb <cyeongb@gmail.com>
2022-04-29 00:51:17 +0900

    게시물 삭제하기 기능 구현

--- frontend/src/components/Pin.jsx
+++ frontend/src/components/Pin.jsx
@@ -1,19 +1,15 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
-import {
-  MdDownloadForOffline,
-  MdDelete,
-  MdSave,
-  MdOutlineCheck,
-} from "react-icons/md";
-import { BsFillArrowUpRightCircleFill } from "react-icons/bs";
+import { MdDownloadForOffline, MdDelete } from "react-icons/md";
+import { IoMdShareAlt } from "react-icons/io";
 import { FaHeart } from "react-icons/fa";
 import { client, urlFor } from "../client";
 import { fetchUser } from "../utils/fetchUser";
 
 const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
   console.log("save >", save);
+  console.log("image >", image);
 
   const navigate = useNavigate();
 
@@ -57,6 +53,13 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
     }
   };
 
+  //게시물 삭제하기
+  const deletePin = (id) => {
+    client.delete(id).then(() => {
+      window.location.reload();
+    });
+  };
+
   return (
     <div className="m-2 ">
       <div
@@ -72,15 +75,16 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
         />
         {postHovered && (
           <div
-            className="absolute top-0 w-full h-full flex flex-col justify-between p-1 pr-2 pt-2 pb-2 z-50"
+            className="absolute top-0 w-full h-full flex flex-col justify-between p-1 pr-2  pb-2 z-50"
             style={{ height: "100%" }}
           >
             <div className="flex items-center justify-between">
               <div className="flex gap-2">
                 <a
-                  className="bg-white w-9 h-9 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none hover:transition-all duration-300 ease-in "
+                  className="bg-white w-9 h-9 rounded-full mb-7 flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none hover:transition-all duration-300 ease-in "
                   href={`${image?.asset?.url}?dl=`}
                   download
+                  title="다운받기"
                   onClick={(e) => e.stopPropagation()}
                   // 이미지detail로 리다이렉트 막기 위해
                 >
@@ -89,12 +93,17 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
               </div>
 
               {alreadySaved ? (
-                <button type="button" className="bg-purple-500 opacity-60 hover:cursor-default hover:opacity-100 text-white font-bold p-2 text-lg rounded-3xl hover:shadow-md outlined-none hover:transition-all duration-300 ease-in">
-                  {save?.length}  <FaHeart />
+                <button
+                  type="button"
+                  title="저장됨"
+                  className="bg-purple-400  opacity-60 hover:cursor-default hover:opacity-100 text-white font-bold p-2 py-3 text-lg rounded-3xl hover:shadow-md outlined-none hover:transition-all duration-300 ease-in"
+                >
+                  {save?.length} <FaHeart />
                 </button>
               ) : (
                 <button
                   type="button"
+                  title="저장하기"
                   onClick={(e) => {
                     e.stopPropagation();
                     savePin(_id);
@@ -105,9 +114,43 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
                 </button>
               )}
             </div>
+
+            <div className="flex justify-between items-center gap-2 w-full">
+              {destination && (
+                <a
+                  href={destination}
+                  target="_blank"
+                  rel="noreferrer"
+                  title="출처"
+                  className="bg-white flex items-center gap-2 text-black font-bold p-2 pl-4 pr-4 rounded-full opacity-70 hover:100 hover:shadow-md"
+                >
+                  <IoMdShareAlt />
+                  {destination.length > 20
+                    ? destination.slice(8, 25)
+                    : destination.slice(8)}
+                </a>
+              )}
+
+              {postedBy?._id === userInfo.googleId && (
+                <button
+                  type="button"
+                  title="삭제하기"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    deletePin(_id);
+                  }}
+                  className="bg-red-400  opacity-60 hover:cursor-default hover:opacity-100 text-white font-bold p-2 py-2 text-lg rounded-3xl hover:shadow-md outlined-none hover:transition-all duration-300 ease-in"
+                >
+                  <MdDelete />
+                </button>
+              )}
+            </div>
           </div>
         )}
       </div>
+
+
+      
     </div>
   );
 };
