80d84d3eb125dfc728bd7dd670a461e39f0c66a4
cyeongb <cyeongb@gmail.com>
2022-05-02 00:33:37 +0900

    categories 임시 데이터 적용

--- frontend/src/utils/data.js
+++ frontend/src/utils/data.js
@@ -1,5 +1,49 @@
 // fetching sanity data
 
+export const categories = [
+  {
+    name: "foods",
+    image:
+      // 치킨
+      "https://i.pinimg.com/564x/a5/c7/7f/a5c77fc0289c6712c4b485a3772ae18c.jpg",
+  },
+  {
+    name: "pet",
+    image:
+      "https://i.pinimg.com/564x/f5/87/e8/f587e89e7f6b0017b0110372528ec0e2.jpg",
+  },
+  {
+    name: "programming",
+    image:
+      "https://i.pinimg.com/564x/5e/2b/a6/5e2ba6045aa43f4588ca400ad469d715.jpg",
+  },
+  {
+    name: "travel",
+    image:
+      "https://i.pinimg.com/564x/d6/e2/31/d6e231ad2e5653a5a3b350662642e9b4.jpg",
+  },
+  {
+    name: "universe",
+    image:
+      "https://i.pinimg.com/736x/5a/3f/13/5a3f13984fd41a54d5fa99222f342219.jpg",
+  },
+  {
+    name: "kpop",
+    image:
+      "https://i.pinimg.com/564x/b8/8c/d1/b88cd1b971bbe8abbdacddde55d3ae7b.jpg",
+  },
+  {
+    name: "ootd",
+    image:
+      "https://i.pinimg.com/736x/42/f8/65/42f8651157094debfacc2f29f1aa12b9.jpg",
+  },
+  {
+    name: "mind",
+    image:
+      "https://i.pinimg.com/736x/24/1b/f2/241bf2b9499b5a4dd8232a3835aaf79e.jpg",
+  },
+];
+
 //사용자 데이터 불러오는 쿼리 함수
 export const userQuery = (userId) => {
   const query = `*[_type =="user" && _id == '${userId}']`;
